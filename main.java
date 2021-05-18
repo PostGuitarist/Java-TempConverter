64076ae62acfc7c5bf41eace623ccd5d9612fd25
Zaden Connell <43675100+PostGuitarist@users.noreply.github.com>
2021-05-18 09:38:24 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,20 +1,41 @@
 import java.util.Scanner;
 
-class Main {  
-
-  public int fahTocel(int x){
+public class Main {  
+  
+  public static void fahrenToCel(double x){
     x-= 32;
     x *= 0.5556;
-    return x;
+    System.out.println(x);
   }
+  
+  public static void celToFah(double y){
+    y *= 1.8;
+    y += 32;
+    System.out.println(y);
+  }  
   
   public static void main(String args[]) { 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Fahrenheit to Celsius");
-    System.out.println("Please enter degrees F as an integer.");
-    int degreesF = scan.nextInt;
-
-    fahTocel(degreesF);
+    int choice = 0;
+    
+    while(choice != 3){
+      System.out.println("Please choose which conversion you want to do: ");
+      System.out.println("1 - Fahrenheit to Celsius *** 2 - Celcius to Fahrenheit *** 3 - Quit");
+      choice = scan.nextInt();
+      if(choice == 1){
+        System.out.println("Fahrenheit to Celsius");
+        System.out.println("Please enter degrees F as an integer.");
+        double degreesF = scan.nextDouble();        
+        fahrenToCel(degreesF);
+      }
+      else if(choice == 2){
+        System.out.println("Celcius to Fahrenheit");
+        System.out.println("Please enter degrees C as an integer.");
+        double degreesC = scan.nextDouble();        
+        celToFah(degreesC);        
+        
+      }  
+    }  
   }
 }
